c847fb22466c6e3a234fee316dc8df10ff5f9438
MedishettyIshwarya <medishettyishwarya114@gmail.com>
2025-10-13 20:59:04 +0530

    test

--- stringduplicate.java
+++ stringduplicate.java
@@ -1,16 +1,43 @@
 //find string duplicates from given array
+
+import java.util.HashSet;
+
 public class stringduplicate {
     public static void main(String[] args) {
-        String arr[]={"ishu","medhi","ishu","abc","xyz","abc"};
-        for(int i=0;i<arr.length;i++){
+        String arr[]={"ishu","medhi","abc","xyz","abc"};
+        /*for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i]==arr[j]){
                     System.out.println("duplicate string is:"+arr[i]);
                 }
                     
             }
-        }
+        }*/
+        //using hashset 
         
+        HashSet <String> dup=new HashSet();
+        /*System.out.println(dup.add("ishu"));
+        System.out.println(dup.add("medi"));
+        System.out.println(dup.add("ishu"));*/
+       //find string duplicates from given array
+
+
+
+       
+        boolean status = false; // <-- Move this here
+
+        for (int i = 0; i < arr.length; i++) {
+            if (dup.add(arr[i]) == false) {
+                System.out.println("duplicate string is:" + arr[i]);
+                status = true;
+            }
+        }
+        if (status == false) {
+            System.out.println("no duplicate string");
+        }
     }
-    
 }
+
+    
+    
+

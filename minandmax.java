f9b876c559cbeeb5d537f006f83f8d1925981fb0
MedishettyIshwarya <medishettyishwarya114@gmail.com>
2025-10-13 19:16:35 +0530

    ishu

--- minandmax.java
+++ minandmax.java
@@ -1,19 +1,28 @@
-//find the min and max from the array
-public class minandmax {
+//find max and min elemnt from given element
+public class minandmax{
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6,7,8,9};
-        int min=arr[0];
+        //max element
+        /*int  arr[]={1,2,4,5,8,10};
         int max=arr[0];
         for(int i=1;i<arr.length;i++){
             if(arr[i]>max){
                 max=arr[i];
-            }else if(arr[i]<min){
+
+            }
+
+        }
+        System.out.println("max element is:"+max);*/
+
+        //min element
+        int arr[]={5,7,9,4,10};
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
                 min=arr[i];
             }
         }
-        System.out.println("min value is:"+min);
-        System.out.println("max value is:"+max);
+        System.out.println("min elemennt:"+min);
+
         
     }
-    
 }
---
+++ missingnumber.java
@@ -0,0 +1,21 @@
+//find missing number from given array
+public class missingnumber {
+    public static void main(String[] args) {
+        int arr[]={1,2,4,5};
+        int sum1=0;
+        for(int i=0;i<arr.length;i++){
+            sum1=sum1+arr[i];
+        }
+        System.out.println("sum of given array:"+sum1);
+        int sum2=0;
+        for(int i=0;i<=5;i++){
+            sum2=sum2+i;
+        }
+        System.out.println("sum of array element:"+sum2);
+        System.out.println(("missing number is:"+(sum2-sum1)));
+    
+
+        
+    }
+    
+}
